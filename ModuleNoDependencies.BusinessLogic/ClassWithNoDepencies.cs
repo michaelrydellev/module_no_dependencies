3adfe611e0f50ea073a668dd9f09cb623807e96f
Michael Rydell <michael.rydell@eagleview.com>
2018-02-13 15:59:56 -0800

    Initial commit

---
+++ ModuleNoDependencies.BusinessLogic/ClassWithNoDepencies.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ModuleNoDependencies.BusinessLogic
+{
+    public class ClassWithNoDepencies
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine("Something.");
+        }
+    }
+}
